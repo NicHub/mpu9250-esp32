719d1b7e0069b208b4301d263cd258e29a069822
NicHub <nicolas.jeanmonod@gmail.com>
2019-07-12 13:59:53 +0200

    réorganisation du code

---
+++ src/imu.cpp
@@ -0,0 +1,129 @@
+/**
+ * Copyright (C) 2019  Nicolas Jeanmonod, ouilogique.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <imu.h>
+
+bool IMU::readMPU9250()
+{
+    // If intPin goes high, all data registers have new data
+    // On interrupt, check if data ready interrupt
+    if (this->readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+    {
+        this->readAccelData(this->accelCount); // Read the x/y/z adc values
+        this->getAres();
+
+        // Now we'll calculate the accleration value into actual g's
+        // This depends on scale being set
+        this->ax = (float)this->accelCount[0] * this->aRes; // - accelBias[0];
+        this->ay = (float)this->accelCount[1] * this->aRes; // - accelBias[1];
+        this->az = (float)this->accelCount[2] * this->aRes; // - accelBias[2];
+
+        this->readGyroData(this->gyroCount); // Read the x/y/z adc values
+        this->getGres();
+
+        // Calculate the gyro value into actual degrees per second
+        // This depends on scale being set
+        this->gx = (float)this->gyroCount[0] * this->gRes;
+        this->gy = (float)this->gyroCount[1] * this->gRes;
+        this->gz = (float)this->gyroCount[2] * this->gRes;
+
+        this->readMagData(this->magCount); // Read the x/y/z adc values
+        this->getMres();
+        // User environmental x-axis correction in milliGauss, should be
+        // automatically calculated
+        this->magbias[0] = +470.;
+        // User environmental x-axis correction in milliGauss TODO axis??
+        this->magbias[1] = +120.;
+        // User environmental x-axis correction in milliGauss
+        this->magbias[2] = +125.;
+
+        // Calculate the magnetometer values in milliGauss
+        // Include factory calibration per data sheet and user environmental
+        // corrections
+        // Get actual magnetometer value, this depends on scale being set
+        this->mx = (float)this->magCount[0] * this->mRes * this->magCalibration[0] -
+                 this->magbias[0];
+        this->my = (float)this->magCount[1] * this->mRes * this->magCalibration[1] -
+                 this->magbias[1];
+        this->mz = (float)this->magCount[2] * this->mRes * this->magCalibration[2] -
+                 this->magbias[2];
+    } // if (readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
+
+    // Must be called before updating quaternions!
+    this->updateTime();
+
+    // Sensors x (y)-axis of the accelerometer is aligned with the y (x)-axis of
+    // the magnetometer; the magnetometer z-axis (+ down) is opposite to z-axis
+    // (+ up) of accelerometer and gyro! We have to make some allowance for this
+    // orientationmismatch in feeding the output to the quaternion filter. For the
+    // MPU-9250, we have chosen a magnetic rotation that keeps the sensor forward
+    // along the x-axis just like in the LSM9DS0 sensor. This rotation can be
+    // modified to allow any convenient orientation convention. This is ok by
+    // aircraft orientation standards! Pass gyro rate as rad/s
+    //  MadgwickQuaternionUpdate(ax, ay, az, gx*PI/180.0f, gy*PI/180.0f, gz*PI/180.0f,  my,  mx, mz);
+    MahonyQuaternionUpdate(this->ax, this->ay, this->az, this->gx * DEG_TO_RAD,
+                           this->gy * DEG_TO_RAD, this->gz * DEG_TO_RAD, this->my,
+                           this->mx, this->mz, this->deltat);
+
+    // Serial print and/or display at 0.5 s rate independent of data rates
+    this->delt_t = millis() - this->count;
+
+    // update LCD once per half-second independent of read rate
+    // if (this->delt_t > 500)
+    bool timeToGo = this->delt_t > 500;
+    if (!timeToGo)
+        return false;
+
+    // Define output variables from updated quaternion---these are Tait-Bryan
+    // angles, commonly used in aircraft orientation. In this coordinate system,
+    // the positive z-axis is down toward Earth. Yaw is the angle between Sensor
+    // x-axis and Earth magnetic North (or true North if corrected for local
+    // declination, looking down on the sensor positive yaw is counterclockwise.
+    // Pitch is angle between sensor x-axis and Earth ground plane, toward the
+    // Earth is positive, up toward the sky is negative. Roll is angle between
+    // sensor y-axis and Earth ground plane, y-axis up is positive roll. These
+    // arise from the definition of the homogeneous rotation matrix constructed
+    // from quaternions. Tait-Bryan angles as well as Euler angles are
+    // non-commutative; that is, the get the correct orientation the rotations
+    // must be applied in the correct order which for this configuration is yaw,
+    // pitch, and then roll.
+    // For more see
+    // http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
+    // which has additional links.
+    this->yaw = atan2(2.0f * (*(getQ() + 1) * *(getQ() + 2) + *getQ() *
+                                                                *(getQ() + 3)),
+                    *getQ() * *getQ() + *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) - *(getQ() + 3) * *(getQ() + 3));
+    this->pitch = -asin(2.0f * (*(getQ() + 1) * *(getQ() + 3) - *getQ() *
+                                                                  *(getQ() + 2)));
+    this->roll = atan2(2.0f * (*getQ() * *(getQ() + 1) + *(getQ() + 2) *
+                                                           *(getQ() + 3)),
+                     *getQ() * *getQ() - *(getQ() + 1) * *(getQ() + 1) - *(getQ() + 2) * *(getQ() + 2) + *(getQ() + 3) * *(getQ() + 3));
+    this->pitch *= RAD_TO_DEG;
+    this->yaw *= RAD_TO_DEG;
+    // Declination of SparkFun Electronics (40°05'26.6"N 105°11'05.9"W) is
+    // 	8° 30' E  ± 0° 21' (or 8.5°) on 2016-07-19
+    // - http://www.ngdc.noaa.gov/geomag-web/#declination
+    this->yaw -= 8.5;
+    this->roll *= RAD_TO_DEG;
+
+    this->count = millis();
+    this->sumCount = 0;
+    this->sum = 0;
+
+    return true;
+}
---
+++ src/lcd.cpp
@@ -0,0 +1,84 @@
+/**
+ * Copyright (C) 2019  Nicolas Jeanmonod, ouilogique.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+
+#include <Arduino.h>
+#include <lcd.h>
+
+void LCDscreen::setup1()
+{
+    // Start device display with ID of sensor
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextColor(WHITE, BLACK); // Set pixel color; 1 on the monochrome screen
+    M5.Lcd.setTextSize(2);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("MPU9250");
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(0, 20);
+    M5.Lcd.print("9-DOF 16-bit");
+    M5.Lcd.setCursor(0, 30);
+    M5.Lcd.print("motion sensor");
+    M5.Lcd.setCursor(20, 40);
+    M5.Lcd.print("60 ug LSB");
+    delay(1000);
+
+    // Set up for data display
+    M5.Lcd.setTextSize(1);    // Set text size to normal, 2 is twice normal etc.
+    M5.Lcd.fillScreen(BLACK); // clears the screen and buffer
+    M5.Lcd.setTextColor(GREEN, BLACK);
+}
+
+void LCDscreen::printLCD()
+{
+    // M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextFont(2);
+
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.print("     x       y       z ");
+    M5.Lcd.setCursor(0, 24);
+    M5.Lcd.printf("% 6d  % 6d  % 6d     mg   \r\n", (int)(1000 * IMU1.ax), (int)(1000 * IMU1.ay), (int)(1000 * IMU1.az));
+    M5.Lcd.setCursor(0, 44);
+    M5.Lcd.printf("% 6d  % 6d  % 6d      o/s  \r\n", (int)(IMU1.gx), (int)(IMU1.gy), (int)(IMU1.gz));
+    M5.Lcd.setCursor(0, 64);
+    M5.Lcd.printf("% 6d  % 6d  % 6d     mG    \r\n", (int)(IMU1.mx), (int)(IMU1.my), (int)(IMU1.mz));
+
+    M5.Lcd.setCursor(0, 100);
+    M5.Lcd.printf("  yaw: % 5.2f    pitch: % 5.2f    roll: % 5.2f   \r\n", (IMU1.yaw), (IMU1.pitch), (IMU1.roll));
+
+    // With these settings the filter is updating at a ~145 Hz rate using the
+    // Madgwick scheme and >200 Hz using the Mahony scheme even though the
+    // display refreshes at only 2 Hz. The filter update rate is determined
+    // mostly by the mathematical steps in the respective algorithms, the
+    // processor speed (8 MHz for the 3.3V Pro Mini), and the magnetometer ODR:
+    // an ODR of 10 Hz for the magnetometer produce the above rates, maximum
+    // magnetometer ODR of 100 Hz produces filter update rates of 36 - 145 and
+    // ~38 Hz for the Madgwick and Mahony schemes, respectively. This is
+    // presumably because the magnetometer read takes longer than the gyro or
+    // accelerometer reads. This filter update rate should be fast enough to
+    // maintain accurate platform orientation for stabilization control of a
+    // fast-moving robot or quadcopter. Compare to the update rate of 200 Hz
+    // produced by the on-board Digital Motion Processor of Invensense's MPU6050
+    // 6 DoF and MPU9150 9DoF sensors. The 3.3 V 8 MHz Pro Mini is doing pretty
+    // well!
+
+    // M5.Lcd.setCursor(0, 60);
+    // M5.Lcd.printf("yaw:%6.2f   pitch:%6.2f   roll:%6.2f  ypr \r\n",(IMU1.yaw), (IMU1.pitch), (IMU1.roll));
+    M5.Lcd.setCursor(12, 144);
+    M5.Lcd.print("rt: ");
+    M5.Lcd.print((float)IMU1.sumCount / IMU1.sum, 2);
+    M5.Lcd.print(" Hz");
+}
